4e351a6c1ddc0eba249f768f12b012f72338008f
Carlos Ruiz <carlosandresgoo@gmail.com>
2023-03-20 08:52:25 -0500

    HTML,CSS Y JS

--- js/farmacia.js
+++ js/farmacia.js
@@ -1,31 +1,54 @@
 const { createApp } = Vue
 
 const app = createApp({
-    data(){
+    data() {
         return {
             // Inicializamos las variables
             datos: [],
             datosFarmacia: [],
-            farmaciaProdcutos:[],
+            datosFarmaciaFiltrados: [{}, {}],
+            isAsideInactive: true,
+            valorBusqueda: "",
         }
     },
-    created(){
-        fetch ("https://mindhub-xj03.onrender.com/api/petshop")
-        .then(response => response.json())
-        .then(data => {
-            this.datos = data;
-            this.datosFarmacia = data.filter(producto => producto.categoria === "farmacia");
-            this.productosFiltrados =  data;
-            console.log(this.datosFarmacia)
-        })
-        .catch(error => console.log(error))
+    created() {
+        fetch("https://mindhub-xj03.onrender.com/api/petshop")
+            .then(response => response.json())
+            .then(data => {
+                this.datos = data;
+                this.datosFarmacia = data.filter(producto => producto.categoria === "farmacia");
+                this.datosFarmaciaFiltrados = data.filter(producto => producto.categoria === "farmacia");
+            })
+            .catch(error => console.log(error))
     },
-    computed : {
-        filtro(){
-            let filtradoBusqueda = this.datos.filter( p => p.producto.toLowerCase().includes( this.valorBusqueda.toLowerCase() ) )
-            this.prodcutossFiltrados = filtradoBusqueda
+    methods: {
+        filtroBusqueda() {
+            this.datosFarmaciaFiltrados = this.datosFarmacia.filter(producto =>
+                producto.producto.toLowerCase().includes(this.valorBusqueda.toLowerCase())
+            )
         },
+        aparecerCarrito() {
+            this.isAsideInactive = !this.isAsideInactive;
         }
+    },
 })
 
 app.mount("#app")
+
+//boton
+const btnScrollTop = document.querySelector('#btn-scroll-top');
+
+btnScrollTop.addEventListener('click', function () {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
+
+window.addEventListener('scroll', function () {
+    if (window.pageYOffset > 200) {
+        btnScrollTop.style.display = 'block';
+    } else {
+        btnScrollTop.style.display = 'none';
+    }
+});
