a1a898e5e9b4ee750a82dd598aa7b95ad86d657b
Santi <jsanti.restrepo.2021@gmail.com>
2023-03-21 17:13:44 -0500

    arreglo el contact

--- js/contact.js
+++ js/contact.js
@@ -1,6 +1,25 @@
 const { createApp } = Vue;
 
 const app = createApp({
+    data(){
+        return {
+            // Inicializamos las variables
+            infoName : "",
+            infoTel : "",
+            infoEmail : "",
+            infoMascota : "",
+            textArea : "",
+        }
+    },
+    methods: {
+        onSubmit() {
+            this.infoName = "";
+            this.infoTel = "";
+            this.infoEmail = "";
+            this.infoMascota = "";
+            this.textArea = "";
+        },
+    }
 })
 
 app.mount("#app");
